fb90976ea07b011206a67912f59343d456001ea6
lyx <739047146@qq.com>
2021-08-24 10:54:40 +0800

    mybatis自动生成代码

---
+++ src/main/java/com/example/domain/Account.java
@@ -0,0 +1,46 @@
+package com.example.domain;
+
+public class Account {
+    private Integer id;
+
+    private Integer uid;
+
+    private Double money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", uid=").append(uid);
+        sb.append(", money=").append(money);
+        sb.append("]");
+        return sb.toString();
+    }
+}
